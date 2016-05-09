9311b81e94393ef7946d65f86b2246c553b065e5
Corin Cody <cacody@umich.edu>
2016-05-09 15:03:30 -0400

    made working dir added draft tpl

---
+++ working/DRAFT.tpl.php
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+<div id="#page">
+	
+	<header id="header" class="bg-image" role="banner">
+		<div id="top-nav-and-icons-region"></div>
+		<div id="logo-name-and-nav-region"></div>
+		<div id="integrated-header-region"></div>	
+		</div>
+	</header>
+	
+	<div id="optional-region-1" role="complementary"></div>
+	
+	<div id="optional-region-2" role="complementary"></div>
+	
+	<main id="content-section"></main>
+	
+	<div id="optional-region-3" role="complementary"></div>
+	
+	<div id="optional-region-4" role="complementary"></div>
+	
+	<footer id="footer">
+		
+</div>
+
+
+
+
+
+</di
+
