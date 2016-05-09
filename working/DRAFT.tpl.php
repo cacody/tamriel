d2a16011f74b614c7f7fdfc19e1987edd0f0af97
Corin Cody <cacody@umich.edu>
2016-05-09 15:14:18 -0400

    draft tpl primary structure

--- working/DRAFT.tpl.php
+++ working/DRAFT.tpl.php
@@ -1,35 +1,73 @@
 
 
-
-
+<?php endif; ?>
 
 
 <div id="#page">
 	
-	<header id="header" class="bg-image" role="banner">
-		<div id="top-nav-and-icons-region"></div>
-		<div id="logo-name-and-nav-region"></div>
-		<div id="integrated-header-region"></div>	
-		</div>
-	</header>
-	
-	<div id="optional-region-1" role="complementary"></div>
-	
-	<div id="optional-region-2" role="complementary"></div>
-	
-	<main id="content-section"></main>
-	
-	<div id="optional-region-3" role="complementary"></div>
-	
-	<div id="optional-region-4" role="complementary"></div>
-	
-	<footer id="footer">
+	<?php if (): ?>
+		<header id="header" class="bg-image" role="banner">
+    	
+			<?php if (): ?>
+				<div id="top-nav-and-icons-region">
+			
+				</div>
+			<?php endif; ?>
+			
+			<?php if (): ?>
+				<div id="logo-name-and-nav-region">
+				
+				</div>
+			<?php endif; ?>
+			
+			<?php if (): ?>
+				<div id="integrated-header-region">
+			
+				</div>	
+			<?php endif; ?>
 		
-</div>
+		</header>
+	<?php endif; ?>
+	
+	<?php if (): ?>
+		<div id="optional-region-1" role="complementary">
+		
+		</div>
+	<?php endif; ?>
+	
+	<?php if (): ?>
+		<div id="optional-region-2" role="complementary">
+		
+		</div>
+	<?php endif; ?>
+	
+	<?php if (): ?>
+		<main id="content-section">
+		
+		</main>
+	<?php endif; ?>
+	
+	<?php if (): ?>
+		<div id="optional-region-3" role="complementary">
+		
+		</div>
+	<?php endif; ?>
+	
+	<?php if (): ?>
+		<div id="optional-region-4" role="complementary">
+		
+		</div>
+	<?php endif; ?>
 
+	<?php if (): ?>
+		<footer id="footer" role="contentinfo">
+		
+		</footer>
+	<?php endif; ?>
+		
+</div><!-- /#page -->
 
+<nav id="menu" role="navigation">
 
-
-
-</di
+</nav>
 
