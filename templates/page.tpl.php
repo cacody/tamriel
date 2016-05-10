7b503a15274eed9c3f5e660cf04b732ca4b91a61
Corin Cody <cacody@umich.edu>
2016-05-10 10:26:17 -0400

    just adding a frickn hr

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -73,6 +73,9 @@
  * @ingroup templates
  */
 ?>
+
+
+<hr>
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
   <div class="<?php print $container_class; ?>">
     <div class="navbar-header">
